9ee813f9d0d6dc1d53eeef072c1162847c3d2153
Pierre-Alexandre Poirier <pa.poirier@gmail.com>
2013-06-27 15:40:36 -0400

    bunch of updates

--- js/tangible.js
+++ js/tangible.js
@@ -37,7 +37,17 @@ $(document).ready(function() {
 		// ---------------------------------------------------------------------------
 		function setContent(lang) {
 			var data = content;
+			
+			var inContainer = "> .container";
+			var inRow = "> .row";
+			
+			// reset content
+			$("section").empty();
+			$("section").html("<div class=\"container\"></div>");
+			$("section#cover").html("<div class=\"container\"><div class=\"site_title\"></div></div>" + 
+											"<div class=\"container\"><div class=\"row\"><div class=\"span6 tagline\"></div><div class=\"span6\"></div></div></div>");
 
+			/*
 			// cover
 			$("#cover").empty();
 			// main content
@@ -49,87 +59,86 @@ $(document).ready(function() {
 			$("#faq").empty();
 			// footer
 			$("#liens").empty();
+			 */
 			
-			//$("section").empty();
-			
-			// ENGLISH ----------------------------------------------------------------
+			// ENGLISH ----------------------------------------------------------------			
 			if(lang == "en_lang") {
 				// cover
-				$("#cover").append('<h1>' + data.cover.site_title + '</h1>');
-				$("#cover").append('<h2>' + data.cover.tagline.en + '</h2>');
+				$("#cover" + inContainer + " > .site_title").append('<h1>' + data.cover.site_title + '</h1>');
+				$("#cover" + inContainer + " > .row > .tagline").append('<h4>' + data.cover.tagline.en + '</h4>');
 				// main content
-				$("#products").append('<h1>' + data.products.title.en + '</h1>');
-				$("#realisations").append('<h1>' + data.realisations.title.en + '</h1>');
-				$("#events").append('<h1>' + data.events.title.en + '</h1>');
-				$("#services").append('<h1>' + data.services.title.en + '</h1>');
-				$("#mission").append('<h1>' + data.mission.title.en + '</h1>');
-				$("#faq").append('<h1>' + data.faq.title.en + '</h1>');
+				$("#products" + inContainer).append('<h1>' + data.products.title.en + '</h1>');
+				$("#realisations" + inContainer).append('<h1>' + data.realisations.title.en + '</h1>');
+				$("#events" + inContainer).append('<h1>' + data.events.title.en + '</h1>');
+				$("#services" + inContainer).append('<h1>' + data.services.title.en + '</h1>');
+				$("#mission" + inContainer).append('<h1>' + data.mission.title.en + '</h1>');
+				$("#faq" + inContainer).append('<h1>' + data.faq.title.en + '</h1>');
 				// footer
 				//$("#liens").append('<h1>' + data.liens.title.en + '</h1>');
 				
 				// paragraphs
 				for (var i = 0; i < data.products.copy.en.length; i++) {
-					$("#products").append('<p>' + data.products.copy.en[i] + '</p>');
+					$("#products" + inContainer).append('<p>' + data.products.copy.en[i] + '</p>');
 				}
 				for (var i = 0; i < data.realisations.copy.en.length; i++) {
-					$("#realisations").append('<p>' + data.realisations.copy.en[i] + '</p>');
+					$("#realisations" + inContainer).append('<p>' + data.realisations.copy.en[i] + '</p>');
 				}
 				for (var i = 0; i < data.events.copy.en.length; i++) {
-					$("#events").append('<p>' + data.events.copy.en[i] + '</p>');
+					$("#events" + inContainer).append('<p>' + data.events.copy.en[i] + '</p>');
 				}
 				for (var i = 0; i < data.services.copy.en.length; i++) {
-					$("#services").append('<p>' + data.services.copy.en[i] + '</p>');
+					$("#services" + inContainer).append('<p>' + data.services.copy.en[i] + '</p>');
 				}
 				for (var i = 0; i < data.mission.copy.en.length; i++) {
-					$("#mission").append('<p>' + data.mission.copy.en[i] + '</p>');
+					$("#mission" + inContainer).append('<p>' + data.mission.copy.en[i] + '</p>');
 				}
 				for (var i = 0; i < data.faq.copy.en.length; i++) {
-					$("#faq").append('<p>' + data.faq.copy.en[i] + '</p>');
+					$("#faq" + inContainer).append('<p>' + data.faq.copy.en[i] + '</p>');
 				}
 			}
 			// FRENCH -----------------------------------------------------------------
 			else {
 				// cover
-				$("#cover").append('<h1>' + data.cover.site_title + '</h1>');
-				$("#cover").append('<h2>' + data.cover.tagline.fr + '</h2>');
+				$("#cover" + inContainer + " > .site_title").append('<h1>' + data.cover.site_title + '</h1>');
+				$("#cover" + inContainer + " > .row > .tagline").append('<h4>' + data.cover.tagline.fr + '</h4>');
 				// main content
-				$("#products").append('<h1>' + data.products.title.fr + '</h1>');
-				$("#realisations").append('<h1>' + data.realisations.title.fr + '</h1>');
-				$("#events").append('<h1>' + data.events.title.fr + '</h1>');
-				$("#services").append('<h1>' + data.services.title.fr + '</h1>');;
-				$("#mission").append('<h1>' + data.mission.title.fr + '</h1>');
-				$("#faq").append('<h1>' + data.faq.title.fr + '</h1>');
+				$("#products" + inContainer).append('<h1>' + data.products.title.fr + '</h1>');
+				$("#realisations" + inContainer).append('<h1>' + data.realisations.title.fr + '</h1>');
+				$("#events" + inContainer).append('<h1>' + data.events.title.fr + '</h1>');
+				$("#services" + inContainer).append('<h1>' + data.services.title.fr + '</h1>');;
+				$("#mission" + inContainer).append('<h1>' + data.mission.title.fr + '</h1>');
+				$("#faq" + inContainer).append('<h1>' + data.faq.title.fr + '</h1>');
 				// footer
 				//$("#liens").append('<h1>' + data.liens.title.fr + '</h1>');
 				
 				// paragraphs
 				for (var i = 0; i < data.products.copy.fr.length; i++) {
-					$("#products").append('<p>' + data.products.copy.fr[i] + '</p>');
+					$("#products" + inContainer).append('<p>' + data.products.copy.fr[i] + '</p>');
 				}
 				for (var i = 0; i < data.realisations.copy.fr.length; i++) {
-					$("#realisations").append('<p>' + data.realisations.copy.fr[i] + '</p>');
+					$("#realisations" + inContainer).append('<p>' + data.realisations.copy.fr[i] + '</p>');
 				}
 				for (var i = 0; i < data.events.copy.fr.length; i++) {
-					$("#events").append('<p>' + data.events.copy.fr[i] + '</p>');
+					$("#events" + inContainer).append('<p>' + data.events.copy.fr[i] + '</p>');
 				}
 				for (var i = 0; i < data.services.copy.fr.length; i++) {
-					$("#services").append('<p>' + data.services.copy.fr[i] + '</p>');
+					$("#services" + inContainer).append('<p>' + data.services.copy.fr[i] + '</p>');
 				}
 				for (var i = 0; i < data.mission.copy.fr.length; i++) {
-					$("#mission").append('<p>' + data.mission.copy.fr[i] + '</p>');
+					$("#mission" + inContainer).append('<p>' + data.mission.copy.fr[i] + '</p>');
 				}
 				for (var i = 0; i < data.faq.copy.fr.length; i++) {
-					$("#faq").append('<p>' + data.faq.copy.fr[i] + '</p>');
+					$("#faq" + inContainer).append('<p>' + data.faq.copy.fr[i] + '</p>');
 				}
 					
 			}
 			
 			// images
 			for (var i = 0; i < data.products.images.length; i++) {
-				$("#products").append('<img src=\"' + data.products.images[i] + '\" alt=\"product image\" title=\"product image\">');
+				$("#products" + inContainer).append('<img src=\"' + data.products.images[i] + '\" alt=\"product image\" title=\"product image\">');
 			}
 			for (var i = 0; i < data.realisations.images.length; i++) {
-				$("#realisations").append('<img src=\"' + data.realisations.images[i] + '\" alt=\"realisations image\" title=\"realisations image\">');
+				$("#realisations" + inContainer).append('<img src=\"' + data.realisations.images[i] + '\" alt=\"realisations image\" title=\"realisations image\">');
 			}
 		};
 	  	
